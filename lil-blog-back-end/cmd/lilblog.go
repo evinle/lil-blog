2b65e32060989624137bc987e872100c259dc8a9
Evin L <imevinle@gmail.com>
2025-03-23 17:07:26 +1000

    setup(back-end): http server & air live reloading

--- lil-blog-back-end/cmd/lilblog.go
+++ lil-blog-back-end/cmd/lilblog.go
@@ -1,10 +1,31 @@
 package main
 
 import (
-    "fmt"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main(){
-    fmt.Println("Hello World!");
-}
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		// w.Write([]byte("welcome"))
+        hwMap  := map[string]string{"hello": "world"}
+        // jsonHwMap, error := json.MarshalIndent(hwMap, "", " ")
+        jsonHwMap, error := json.Marshal(hwMap)
+
+        if(error != nil){
+            panic(error)
+        } 
+
+        w.Header().Set("Content-Type", "application/json")
+        w.Write(jsonHwMap)
+
+	})
+
+	http.ListenAndServe(":3000", r)
+}
